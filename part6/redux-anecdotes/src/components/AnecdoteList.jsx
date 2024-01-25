b31ccef014aa1f1e4ab7256c64b66862b7681be7
arvuti <arvuti@arvutis-MacBook-Pro.local>
2024-01-25 19:48:01 +0200

    solutions for 6.3 - 6.8

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,26 @@
+import { useDispatch, useSelector} from 'react-redux'
+import { addLike } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+
+  const vote = (id) => {
+    dispatch(addLike(id))
+  }
+  return(
+  anecdotes.map(anecdote =>
+    <div key={anecdote.id}>
+      <div>
+        {anecdote.content}
+      </div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={() => vote(anecdote.id)}>vote</button>
+      </div>
+    </div>
+  ))
+}
+
+
+export default AnecdoteList
